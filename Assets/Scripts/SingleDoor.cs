733055e5d56dde7c985bc6a81cd77b21188e4729
ShadowMole <steven.bruman@yahoo.com>
2018-03-24 19:28:31 -0400

    Fixed Door Detection

--- Assets/Scripts/SingleDoor.cs
+++ Assets/Scripts/SingleDoor.cs
@@ -6,10 +6,17 @@ public class SingleDoor : MonoBehaviour {
 
 	private int state = 0;
 	public float openCount = 0;
+	private float previousTime = 0;
 	private Vector3[] closed = new Vector3[2];
 	private Vector3[] open = new Vector3[2];
+	private Vector3 door1;
+	private Vector3 door2;
+
 	// Use this for initialization
 	void Start () {
+		door1 = GetComponentsInChildren<Transform> () [1].position;
+		door2 = GetComponentsInChildren<Transform> () [2].position;
+
 		closed [0] = new Vector3 (transform.position.x + .825f, 0f, transform.position.z + 0f);
 			closed[1]  = new Vector3 (transform.position.x + -.825f, 0f,transform.position.z +  0f) ;
 		open [0] = new Vector3 (transform.position.x + 2.475f, 0f, transform.position.z + .1f);
@@ -19,15 +26,14 @@ public class SingleDoor : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Debug.Log (state);
+
 		switch (state){
 		case 0:
 			Vector3 pos = this.transform.position;
-			float position = Mathf.Sqrt ((pos.x * pos.x) + (pos.z * pos.z));
 			GameObject[] people = GameObject.FindGameObjectsWithTag ("Person");
 			for (int i = 0; i < people.Length; i++) {
 				Vector3 person = people [i].transform.position;
-				float per = Mathf.Sqrt ((person.x * person.x) + (person.z * person.z));
-				if (Mathf.Abs (position - per) < 5) {
+				if (Mathf.Abs (pos.x - person.x) < 5 && Mathf.Abs (pos.y - person.y) < 5) {
 					state = (state + 1) % 4;
 					break;
 				}
@@ -37,27 +43,28 @@ public class SingleDoor : MonoBehaviour {
 			Transform[] transforms = this.GetComponentsInChildren<Transform> ();
 			if (transforms [1].position.x >= open [0].x || transforms [2].position.x <= open [1].x) {
 				state = (state + 1) % 4;
-				openCount = 5.0f * Time.deltaTime;
+				openCount = Time.fixedTime;
 			} else {
 				transforms [1].position = new Vector3 (transforms [1].position.x + (1.65f / Time.deltaTime), transforms [1].position.y, transforms [1].position.z + (.1f / Time.deltaTime));
 				transforms [2].position = new Vector3 (transforms [2].position.x + (-1.65f / Time.deltaTime), transforms [2].position.y, transforms [2].position.z + (.1f / Time.deltaTime)); 
 			}
 			break;
 		case 2:
-			if (openCount > 0) {
-				openCount--;
-			} else {
+			Debug.Log (Time.fixedTime );
+			Debug.Log (openCount );
+			Debug.Log (Time.fixedTime - openCount);
+			if(Time.fixedTime - openCount  > 10){
 				state = (state + 1) % 4;
+				previousTime = Time.fixedTime;
 			}
 			break;
 		case 3:
 			Transform[] transforms2 = this.GetComponentsInChildren<Transform> ();
-			if (transforms2 [1].position.x <= closed [0].x || transforms2 [2].position.x >= closed [1].x ){
+			if (Time.fixedTime - previousTime  > 10 && ( transforms2 [1].position.x <= closed [0].x || transforms2 [2].position.x >= closed [1].x) ){
 				state = (state + 1) % 4;
-				openCount = 5 * Time.deltaTime;
 			} else {
-				transforms2 [1].position = new Vector3 (transforms2 [1].position.x - (1.65f / Time.deltaTime), transforms2 [1].position.y, transforms2 [1].position.z - (.1f / Time.deltaTime));
-				transforms2 [2].position = new Vector3 (transforms2 [2].position.x - (-1.65f / Time.deltaTime), transforms2 [2].position.y, transforms2 [2].position.z - (.1f / Time.deltaTime)); 
+				transforms2 [1].position = door1;
+				transforms2 [2].position = door2; 
 			}
 			break;
 	}
