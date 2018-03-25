7626d7a202367f963cff72b28f4b628ee0e8808b
ShadowMole <steven.bruman@yahoo.com>
2018-03-24 22:42:14 -0400

    Fixed Teleport Script

--- Assets/Scripts/InLevelTeleport.cs
+++ Assets/Scripts/InLevelTeleport.cs
@@ -5,32 +5,44 @@ using UnityEngine;
 public class InLevelTeleport : MonoBehaviour {
 
 	private float openCount;
+	private GameObject tele1;
+	private GameObject tele2;
+	private GameObject tele3;
+	private GameObject tele4;
 
-	// Use this for initialization
+
+
+
+
 	void Start () {
 		openCount = 0;
+		tele1 = GameObject.FindGameObjectWithTag ("Tele1");
+		tele2 = GameObject.FindGameObjectWithTag ("Tele2");
+		tele3 = GameObject.FindGameObjectWithTag ("Tele3");
+		tele4 = GameObject.FindGameObjectWithTag ("Tele4");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Time.fixedTime - openCount > 10) {
-			if (Mathf.Abs (GameObject.FindGameObjectWithTag("Tele1").transform.position.x - transform.position.x) < .5 && Mathf.Abs (GameObject.FindGameObjectWithTag("Tele1").transform.position.z) < .5) {
-				transform.position = GameObject.FindGameObjectWithTag("Tele2").transform.position;
-				transform.rotation = GameObject.FindGameObjectWithTag("Tele2").transform.rotation;
+			if (Mathf.Abs (tele1.transform.position.x - transform.position.x) < 1 && Mathf.Abs (tele1.transform.position.z - transform.position.z) < 1) {
+				this.transform.position = tele2.transform.position;
+				this.transform.rotation = tele2.transform.rotation;
 				openCount = Time.fixedTime;
-			}else if (Mathf.Abs (GameObject.FindGameObjectWithTag("Tele2").transform.position.x - transform.position.x) < .5 && Mathf.Abs (GameObject.FindGameObjectWithTag("Tele2").transform.position.z) < .5) {
-				transform.position = GameObject.FindGameObjectWithTag("Tele3").transform.position;
-				transform.rotation = GameObject.FindGameObjectWithTag("Tele3").transform.rotation;
+			}else if (Mathf.Abs (tele2.transform.position.x - this.transform.position.x) < 1 && Mathf.Abs (tele1.transform.position.z - this.transform.position.z) < 1) {
+				this.transform.position = tele3.transform.position;
+				this.transform.rotation = tele3.transform.rotation;
 				openCount = Time.fixedTime;
-			}else if (Mathf.Abs (GameObject.FindGameObjectWithTag("Tele3").transform.position.x - transform.position.x) < .5 && Mathf.Abs (GameObject.FindGameObjectWithTag("Tele3").transform.position.z) < .5) {
-				transform.position = GameObject.FindGameObjectWithTag("Tele4").transform.position;
-				transform.rotation = GameObject.FindGameObjectWithTag("Tele4").transform.rotation;
+			}else if (Mathf.Abs (tele3.transform.position.x - this.transform.position.x) < 1 && Mathf.Abs (tele3.transform.position.z - this.transform.position.z) < 1) {
+				this.transform.position = tele4.transform.position;
+				this.transform.rotation = tele4.transform.rotation;
 				openCount = Time.fixedTime;
-			}else if (Mathf.Abs (GameObject.FindGameObjectWithTag("Tele4").transform.position.x - transform.position.x) < .5 && Mathf.Abs (GameObject.FindGameObjectWithTag("Tele4").transform.position.z) < .5) {
-				transform.position = GameObject.FindGameObjectWithTag("Tele1").transform.position;
-				transform.rotation = GameObject.FindGameObjectWithTag("Tele1").transform.rotation;
+			}else if (Mathf.Abs (tele4.transform.position.x - this.transform.position.x) < 1 && Mathf.Abs (tele4.transform.position.z - this.transform.position.z) < 1) {
+				this.transform.position = tele1.transform.position;
+				this.transform.rotation = tele1.transform.rotation;
 				openCount = Time.fixedTime;
 			}	
 		}
 	}
 }
+
